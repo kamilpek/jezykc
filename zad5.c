177e51c1897cd897b1ad5b13ad62fad026f6d010
Kamil Pek <kpek@p210-7.labpk.inf.ug.edu.pl>
2014-10-26 11:17:24 +0100

    26.10 11:15

---
+++ zad5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+int a;
+int b;
+
+printf("Podaj A: \n");
+scanf("%d", &a);
+
+printf("Podaj B: \n");
+scanf("%d", &b);
+
+if(a>0){ 
+if(b>0){
+printf("A i B są dodadtnie\n");
+}}
+else{
+printf("A i B są ujemne\n");
+}
+
+return 0;
+
+}
+
