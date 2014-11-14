e60d8b443a06214a64c30932bb70b66fb20f7a1f
Kamil Pek <kamil.pek@gmail.com>
2014-11-14 19:18:01 +0100

    postepy

---
+++ 2014-11-09/zad11.c
@@ -0,0 +1,17 @@
+// Napisz program, ktory pyta uzytkownika o napis, a nastepnie sprawdza,
+// czy w tym napisie wystepuje napis "gra". Sprawdz swoj program np.
+// dla "gorna granica programowania".
+
+#include <stdio.h>
+
+int main(){
+
+	char napis[20];
+
+	printf("Podaj napis.\n");
+	scanf("%s", napis);
+
+	printf("Napis= %s\n", napis);
+
+return 0;
+}
---
+++ 2014-11-09/zad13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+	int akwamaryna[3][3][3] = {127, 255, 212};
+	// char ceglasty[3][3][3] = 235,107,5;
+	// char kanarkowy[3][3][3] = 255,255,51;
+	// char mysi[3][3][3] = 227,231,230;
+	// char piwny[3][3][3] = 253,228,86;
+
+	printf("Akwamaryna: %d\n", akwamaryna);
+	
+return 0;
+}
