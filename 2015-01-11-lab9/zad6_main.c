44d419e17542ed186ffa959a06732ac7b8c122dd
Kamil Pek <kamil.pek@gmail.com>
2015-01-11 10:31:30 +0100

    laczenie plikow zrodlowych

--- 2015-01-11-lab9/zad6_main.c
+++ 2015-01-11-lab9/zad6_main.c
@@ -4,7 +4,16 @@
 #include "zad6_listy.h"
 
 // Kompilacja programu ktory sklada sie z wielu plikow zrodlowych
+// 
+// 1. Kompilujemy wszystko na raz i od nowa
 // gcc zad6_main.c zad6_listy.c -o zad6
+//
+// 2. Kompilujemy poszczegolne pliki - tworzymy pliki obiektowe
+// gcc zad6_main.c -c // stworzyl plik zad6_main.o
+// gcc zad6_listy.c -c // stworzyl plik zad6_main.o
+//
+// 3. Konsolidacja - laczenie plikow obiektowych
+// gcc zad6_main.o zad6_listy.o -o zad6.out
 
 int main()
 {
